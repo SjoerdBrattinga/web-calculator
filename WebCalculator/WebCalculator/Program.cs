6438e6638fc42dc52a4c8c316397d6555d4c0d00
Sjoerd Brattinga <sjoerd-j-b@hotmail.com>
2023-05-08 20:00:40 +0200

    Added dependencies

--- WebCalculator/WebCalculator/Program.cs
+++ WebCalculator/WebCalculator/Program.cs
@@ -1,6 +1,11 @@
 using WebCalculator;
+using FluentValidation;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using WebCalculator.Validations;
+using WebCalculator.Service;
+using WebCalculator.Domain.Interfaces;
+using WebCalculator.Domain.Operations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +13,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddValidatorsFromAssemblyContaining<CalculationRequestValidation>();
+builder.Services.AddScoped<ICalculatorService, CalculatorService>();
 
-//builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
-//{
-// Returns double.NaN and double.Infinity
-//    options.SerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+builder.Services.AddTransient<IOperation, Addition>();
+builder.Services.AddTransient<IOperation, Subtraction>();
+builder.Services.AddTransient<IOperation, Multiplication>();
+builder.Services.AddTransient<IOperation, Division>();
+builder.Services.AddTransient<IOperation, Square>();
+builder.Services.AddTransient<IOperation, SquareRoot>();
+builder.Services.AddTransient<IOperation, Exponent>();
+builder.Services.AddSingleton<Func<IEnumerable<IOperation>>>
+    (x => () => x.GetService<IEnumerable<IOperation>>()!);
+builder.Services.AddSingleton<IOperationFactory, OperationFactory>();
 
-//});
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    /// Returns double.NaN and double.Infinity etc
+    options.SerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+
+});
 
 //builder.Services.Configure<ApiBehaviorOptions>(options =>
 //{
@@ -36,6 +55,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.ConfigureBasicApi();
 app.ConfigureApi();
 
 app.Run();
