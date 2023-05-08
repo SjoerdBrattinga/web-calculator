6184abc3308d6353e6aa46af0215a71c36d3b2c5
Sjoerd Brattinga <sjoerd-j-b@hotmail.com>
2023-05-08 19:56:58 +0200

    Added service layer

---
+++ WebCalculator/WebCalculator.Service/CalculatorService.cs
@@ -0,0 +1,143 @@
+﻿using WebCalculator.Domain.Interfaces;
+using WebCalculator.Domain.Models;
+using WebCalculator.Domain.Operations;
+
+namespace WebCalculator.Service
+{
+    public class CalculatorService : ICalculatorService
+    {
+        private readonly IOperationFactory _operationFactory;
+
+        public CalculatorService(IOperationFactory operationFactory)
+        {
+            _operationFactory = operationFactory;
+        }
+
+        public CalculatorResult Calculate(Calculation calculation)
+        {
+            double result = 0;
+            try
+            {
+                var operation = _operationFactory.Create(calculation.Operator);
+                
+
+                if (operation is IBinaryOperation)
+                {
+
+                }
+                else if (operation is IUnaryOperation) 
+                {
+
+                }
+
+                //result.Result = operation.Calculate();
+                //result.Operation = operation;
+
+                return CalculatorResult.Success(result);
+            }
+            catch (Exception ex)
+            {                
+                return CalculatorResult.Failure(ex.Message);
+            }
+
+            //CalculatorResult result = new()
+            //{
+            //    IsSuccess = true
+            //};          
+
+            //try
+            //{
+            //    var operation = OperationFactory.CreateOperation(calculation);
+
+            //    result.Result = operation.Calculate();
+            //    result.Operation = operation;
+
+            //    return result;
+            //}
+            //catch (Exception ex)
+            //{
+            //    result.IsSuccess = false;
+            //    result.ErrorMessage = ex.Message;
+
+            //    return result;
+            //}            
+        }
+
+  
+
+            //public Calculation PerformCalculation(Calculation calculation)
+            //{
+            //    if (calculation is null)
+            //    {
+            //        throw new ArgumentNullException(nameof(calculation));
+            //    }
+
+            //    if(calculation.Operand2.HasValue)
+            //    {
+            //        calculation.Result = calculation.Operator switch
+            //        {
+            //            "+" => (double)(calculation.Operand1 + calculation.Operand2.Value),
+            //            "-" => (double)(calculation.Operand1 - calculation.Operand2.Value),
+            //            "*" => (double)(calculation.Operand1 * calculation.Operand2.Value),
+            //            "/" => (double)(calculation.Operand1 / calculation.Operand2.Value),
+            //            "%" => Percentage(calculation.Operand1, calculation.Operand2.Value),
+            //            "^" => Power(calculation.Operand1, calculation.Operand2.Value),
+            //            _ => null
+            //        }; 
+            //    }
+            //    else
+            //    {
+            //        calculation.Result = calculation.Operator switch
+            //        {
+            //            "+-" => Negate(calculation.Operand1),
+            //            "sqr" => Square(calculation.Operand1),
+            //            "sqr_r" => SquareRoot(calculation.Operand1),
+            //            _ => null
+            //        };
+
+            //    }
+
+            //    return calculation;
+            //}
+
+            //private IOperation GetOperation(string operatorType)
+            //{
+            //    switch (operatorType)
+            //    {
+            //        case "+":
+            //            return new Addition();
+            //        case "-":
+            //            return new SubtractionOperation();
+            //        case "*":
+            //            return new MultiplicationOperation();
+            //        case "/":
+            //            return new DivisionOperation();
+            //        case "sqrt":
+            //            return new SquareRootOperation();
+            //        default:
+            //            return null;
+            //    }
+            //}
+
+
+            //public OperationType GetOperationType(string @operator)
+            //{
+            //    return @operator switch
+            //    {
+            //        "+" => OperationType.Addition,
+            //        "-" => OperationType.Substaction,
+            //        "*" => OperationType.Multiplication,
+            //        "/" => OperationType.Division,
+            //        "sqr" => OperationType.Square,
+            //        "sqr_r" => OperationType.SquareRoot,
+            //        "%" => OperationType.Percentage,
+            //        "^" => OperationType.Exponent,
+            //        _ => OperationType.Invalid,
+            //    };
+            //}
+
+
+   
+
+    }
+}
---
+++ WebCalculator/WebCalculator.Service/OperationFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCalculator.Domain.Interfaces;
+using WebCalculator.Domain.Models;
+using WebCalculator.Domain.Operations;
+
+namespace WebCalculator.Service;
+
+public class OperationFactory : IOperationFactory
+{
+    private readonly Func<IEnumerable<IOperation>> _factory;
+
+    public OperationFactory(Func<IEnumerable<IOperation>> factory)
+    {
+        _factory = factory;
+    }
+    public IOperation Create(string operatorType)
+    {
+        var set = _factory();
+        IOperation operation = set.Where(x => x.OperatorType == operatorType.ToLower()).First();
+        return operation;
+    }
+    //public static IOperation CreateOperation(Calculation calculation)
+    //{  
+    //    if (!calculation.Operand2.HasValue)
+    //    {
+    //        return CreateUnaryOperation(calculation.Operand1, calculation.Operator);
+    //    }
+    //    else
+    //    {
+    //        return CreateBinaryOperation(calculation.Operand1, calculation.Operand2.Value, calculation.Operator);
+    //    }
+    //}
+
+    //public static IOperation GetOperation(string operatorType)
+    //{
+    //    return operatorType.ToLower() switch
+    //    {
+    //        "+" => new Addition(),
+    //        "-" => new Subtraction(),
+    //        "*" => new Multiplication(),
+    //        "/" => new Division(),
+    //        "^" => new Exponent(),
+    //        "sqr" => new Square(),
+    //        "sqrt" => new SquareRoot(),
+    //        //"+-" => new Negate(),
+    //        _ => throw new ArgumentException("Invalid operator type"),
+    //    };
+    //}
+
+    //private static IUnaryOperation CreateUnaryOperation(double operand, string operatorType)
+    //{
+    //    return operatorType.ToLower() switch
+    //    {
+    //        "sqr"  => new Square { Operand = operand },
+    //        "sqrt" => new SquareRoot { Operand = operand },
+    //        //"+-"   => new Negate { Operand = operand },
+    //        _ => throw new ArgumentException("Invalid operator type"),
+    //    };
+    //}
+
+    //private static IOperation CreateBinaryOperation(double operand1, double operand2, string operatorType)
+    //{
+    //    return operatorType.ToLower() switch
+    //    {
+    //        "+" => new Addition { Operand1 = operand1, Operand2 = operand2 },
+    //        "-" => new Subtraction { Operand1 = operand1, Operand2 = operand2 },
+    //        "*" => new Multiplication { Operand1 = operand1, Operand2 = operand2 },
+    //        "/" => new Division { Operand1 = operand1, Operand2 = operand2 },
+    //        "^" => new Exponent { Operand1 = operand1, Operand2 = operand2 },
+    //        _ => throw new ArgumentException("Invalid operator type"),
+    //    };
+    //}
+
+}
